f5442db86ed2539fea9b979cfb8b8a112ca9f83b
mobiusinf <yeshch@mail2.sysu.edu.cn>
2021-12-24 16:10:42 +0000

    first commit

---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,13 @@
+import gym
+from IPython import display
+import matplotlib
+import matplotlib.pyplot as plt
+env = gym.make('Breakout-v0')
+env.reset()
+img = plt.imshow(env.render(mode='rgb_array')) # only call this once
+for _ in range(100):
+    img.set_data(env.render(mode='rgb_array')) # just update the data
+    display.display(plt.gcf())
+    display.clear_output(wait=True)
+    action = env.action_space.sample()
+    env.step(action)
